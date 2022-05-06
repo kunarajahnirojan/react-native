d024b47d3af78f7318e7ee8cde3afe4505d84704
Kunarajah Nirojan <kunarajahnirojan@gmail.com>
2022-05-06 19:42:08 +0530

    configurations added

---
+++ app/screens/Contact/index.js
@@ -0,0 +1,16 @@
+//import liraries
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import styles from "./styles";
+
+// create a component
+const Index = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Contact</Text>
+    </View>
+  );
+};
+
+//make this component available to the app
+export default Index;
---
+++ app/screens/Service/styles.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  // define your styles
+
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2c3e50",
+  },
+});
